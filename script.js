cc8baade5eebd01cfd51ac92944a6ffa6bedeee4
Balint Handler <handlerbalint@gmail.com>
2021-03-21 19:59:48 +0100

    visual feedback on click/keypress

--- script.js
+++ script.js
@@ -18,5 +18,36 @@ function divide(operand1,operand2) {
 function percent(operand) {
   return operand / 100;
 }
-// /BASIC FUNCTIONS
 
+
+// Mouse Click Watcher
+function clickWatcher() {
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach(button => {
+    button.addEventListener('click', event => {
+      console.log(button.value);
+      return button.value;
+    })
+  })
+}
+
+// Keyboard Watcher
+function keyboardWatcher() {
+  window.addEventListener('keydown', key => {
+    const button = document.querySelector(`button[data-key="${key.keyCode}"]`);
+    if (!button) return;
+    console.log(button.value);
+    button.classList.add('buttonactive');
+  })
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach(btn => btn.addEventListener('transitionend', removeTransition));
+  
+ }
+
+// Remove Transition on buttons
+function removeTransition(e) {
+  this.classList.remove('buttonactive');
+} 
+
+clickWatcher()
+keyboardWatcher();
